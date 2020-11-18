1b3ddb0f2b56f69fa79a1fedbd9e4550e2b28879
Benoît Mayer <benoit@benoits-mini.home>
2020-11-18 23:47:51 +0100

    Corrected ft_putendl

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: bmayer <mayer.benoit@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/12 19:09:10 by bmayer            #+#    #+#             */
-/*   Updated: 2020/11/18 23:42:40 by bmayer           ###   ########.fr       */
+/*   Updated: 2020/11/18 23:47:28 by bmayer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 
 void	ft_putendl_fd(char *s, int fd)
 {
+	char c;
+
 	if (!s)
 		return ;
 	write(fd, s, ft_strlen(s));
-	write(fd, '\n', 1);
+	c = '\n';
+	write(fd, &c, 1);
 }
